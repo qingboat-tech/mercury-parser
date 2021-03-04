e2e80b0b4b29ed2baa0262fa378aa959c0f60ff7
jerryan9999 <jerry.an@rentx.io>
2021-03-04 15:07:52 +0800

    build new for shimo

--- dist/mercury.js
+++ dist/mercury.js
@@ -5799,6 +5799,30 @@ var WwwWoshipmComExtractor = {
   }
 };
 
+var ShimoImExtractor = {
+  domain: 'shimo.im',
+  title: {
+    selectors: ["title", ["ql-title-box", "data-value"]]
+  },
+  author: null,
+  date_published: null,
+  dek: null,
+  lead_image_url: null,
+  content: {
+    selectors: [["div.ql-editor"]],
+    defaultCleaner: false,
+    // Is there anything in the content you selected that needs transformed
+    // before it's consumable content? E.g., unusual lazy loaded images
+    transforms: {
+      'ol': 'ul'
+    },
+    // Is there anything that is in the result that shouldn't be?
+    // The clean selectors will remove anything that matches from
+    // the result
+    clean: []
+  }
+};
+
 
 
 var CustomExtractors = /*#__PURE__*/Object.freeze({
@@ -5936,7 +5960,8 @@ var CustomExtractors = /*#__PURE__*/Object.freeze({
   PitchforkComExtractor: PitchforkComExtractor,
   BiorxivOrgExtractor: BiorxivOrgExtractor,
   EpaperZeitDeExtractor: EpaperZeitDeExtractor,
-  WwwWoshipmComExtractor: WwwWoshipmComExtractor
+  WwwWoshipmComExtractor: WwwWoshipmComExtractor,
+  ShimoImExtractor: ShimoImExtractor
 });
 
 var Extractors = _Object$keys(CustomExtractors).reduce(function (acc, key) {
